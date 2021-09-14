eab394cbb1798e000a48551fad9bed58465a4bb5
Ganaraj <ganarajpr@gmail.com>
2021-09-14 18:04:35 +0100

    fix multiple langs

--- components/TranscriptInput.js
+++ components/TranscriptInput.js
@@ -1,5 +1,5 @@
 import { Button, Input, Select, Icon } from 'semantic-ui-react';
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import Sanscript from '@sanskrit-coders/sanscript';
 export default function TranscriptInput (props) {
     const inputOptions = [
@@ -18,28 +18,27 @@ export default function TranscriptInput (props) {
 
     const dispatchChange = () => {
         try{
+            console.log(textInput, inputMethod, outputMethod);
             const val = Sanscript.t(textInput,inputMethod, outputMethod);
             props.onChange(val);
-            console.log(val, textInput);
         }
         catch(e) {
             console.log(e);
         }
     }
+    useEffect(() => {
+        dispatchChange(); // using camelCase for variable name is recommended.
+      }, [textInput, inputMethod, outputMethod]);
 
-    const onInputOptionChange = (e) => {
-        setInputMethod(e.target.value);
-        dispatchChange();
+    const onInputOptionChange = (e, {value}) => {
+        console.log(e, 'input option', value);
+        setInputMethod(value);
     };
-    const onOutputOptionChange = (e) => {
-        setOutputMethod(e.target.value);
-        dispatchChange();
-
+    const onOutputOptionChange = (e, {value}) => {
+        setOutputMethod(value);
     };    
     const onTextChange = (e) => {
         setTextInput(e.target.value);
-        console.log(e.target.value);
-        dispatchChange();
     };
     return (
         <Input type='text' placeholder='Add Word...'>
