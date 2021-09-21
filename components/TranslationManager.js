5c88d84fde6586ca142c43dd3946223d18694aff
Ganaraj <ganarajpr@gmail.com>
2021-09-21 22:02:09 +0100

    delete if inserter

--- components/TranslationManager.js
+++ components/TranslationManager.js
@@ -1,12 +1,18 @@
 import styled from "styled-components";
 import { Label, Button, Icon, Grid, Comment } from 'semantic-ui-react'
 import { useState } from 'react';
+import { signIn, useSession } from 'next-auth/client';
 
 const TranslationManager = (props) => {
     const { translation, onDelete, word } = props;
+    const [session] = useSession();
 
     const handleDeleteTranslation = () => {
         onDelete(word._id, translation.text);
+    };
+
+    const isDeletable = () => {
+        return session?.user?.id === translation.createdBy;
     };
     return (        
             <Comment>
@@ -20,7 +26,7 @@ const TranslationManager = (props) => {
                     <Comment.Actions>
                     {/* <Comment.Action>Upvote</Comment.Action>
                     <Comment.Action>Downvote</Comment.Action> */}
-                    <Comment.Action onClick={handleDeleteTranslation}>Delete</Comment.Action>
+                    { isDeletable() ? <Comment.Action onClick={handleDeleteTranslation}>Delete</Comment.Action> : null }
                     </Comment.Actions>
                 </Comment.Content>
             </Comment>)    
