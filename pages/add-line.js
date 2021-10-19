a69c436b55f23a9c92766f3b074feb794726b92e
ganarajpr <ganaraj.permunda@graingerglobalonline.com>
2021-10-19 15:49:23 +0530

    adding react hook form

--- pages/add-line.js
+++ pages/add-line.js
@@ -2,8 +2,11 @@ import { useState } from "react";
 import { addLine } from '../fetches/line';
 import { getSession } from 'next-auth/client';
 import AccessDenied from '../components/accessDenied';
-import { Container, Segment, Form, Message, Dropdown } from "semantic-ui-react";
+import { Form, Message, Dropdown } from "semantic-ui-react";
 import Layout from '../components/Layout';
+import Container from '@mui/material/Container';
+import AddLineForm from "../components/AddLineForm";
+
 const scriptOptions = [
   { key: 'devanagari', text: 'Devanagari', value: 'devanagari' },
   { key: 'roman', text: 'Roman', value: 'roman' }
@@ -29,46 +32,6 @@ export default function Line({session}) {
   let errorMessage = 'Have you filled all required fields, Is the content in devanagari script?';
 
   if (!session) { return  <Layout><AccessDenied/></Layout> }
-  const [script, setScript] = useState('devanagari');
-  const [language, setLanguage] = useState('sanskrit');
-  const [line, setLine] = useState('');
-  const [book, setBook] = useState('');
-  const [bookContext, setBookContext] = useState('');
-  const [showMessage, setShowMessage] = useState(false);
-
-  const onScriptChange = (e, {value}) => {
-    setScript(value);
-  };
-
-  const onLineChange = (e, {value}) => {
-    setLine(value);
-  };
-
-  const onLanguageChange = (e, {value}) => { setLanguage(value); };
-
-  const onBookChange = (e, {value}) => { setBook(value); };
-
-  const onBookContextChange = (e, {value}) => {
-    setBookContext(value);
-  };
-  const handleDismiss = () => { setShowMessage(false); };
-
-
-  const getMessage = () => {
-    if(positive) {
-      return (<Message positive
-        onDismiss={handleDismiss}
-        header='Submission Success!'
-        content='The paragraph was submitted successfully.'
-      />);
-    }
-    return (<Message negative
-      onDismiss={handleDismiss}
-      header='Something went wrong!'
-      content={errorMessage}
-    />);    
-    
-  };
 
 
   const onSubmit = async () => {
@@ -97,42 +60,9 @@ export default function Line({session}) {
 
   return (
     <Layout>
-      <Container>
-        <Segment>
-            <Form onSubmit={onSubmit}>
-              { showMessage ? getMessage() : null}
-              <Form.Field required inline>
-                <Dropdown
-                  inline
-                  options={scriptOptions}
-                  onChange={onScriptChange}
-                  defaultValue={scriptOptions[0].value}
-                />
-                <Dropdown
-                  inline
-                  options={languageOptions}
-                  onChange={onLanguageChange}
-                  defaultValue={languageOptions[0].value}
-                />                
-                <label>&nbsp;&nbsp;Paragraph</label> 
-                <Form.TextArea rows={10} placeholder="1 Paragraph or Shloka or Mantra" value={line} 
-                    onChange={onLineChange}>
-                </Form.TextArea>                
-              </Form.Field>
-              <Form.Field  required>
-                <label>Book</label>
-                <Form.Input placeholder="Book it belongs to" value={book} onChange={onBookChange}>
-                </Form.Input>
-              </Form.Field>
-              <Form.Field  required>
-                <label>Chapter - Verse - Hymn - Shloka in Book</label>
-                <Form.Input placeholder="ex: 10.3.1" value={bookContext} onChange={onBookContextChange}>
-                </Form.Input>                  
-              </Form.Field>
-              <Form.Button primary>Add Paragraph</Form.Button>
-            </Form>
-        </Segment>   
-      </Container>      
+        <Container maxWidth="sm">
+            <AddLineForm/>
+        </Container>      
     </Layout>    
   );
 }
