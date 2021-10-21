8da847c439aa26049b08a6445cf29be3c3a821c1
ganarajpr <ganaraj.permunda@graingerglobalonline.com>
2021-10-21 19:58:23 +0530

    fix cross origin

--- pages/_app.js
+++ pages/_app.js
@@ -8,7 +8,7 @@ export default function App ({ Component, pageProps }) {
     <>
       <Head>
       <link rel="preconnect" href="https://fonts.googleapis.com"/>
-      <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin/>
+      <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin/>
       <link href="https://fonts.googleapis.com/css2?family=Vesper+Libre&display=swap" rel="stylesheet"/>
       </Head>
       <CssBaseline>
