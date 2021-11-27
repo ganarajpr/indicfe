612073b6a881d6918670c3de5b9e691846589b16
ganarajpr <ganarajpr@gmail.com>
2021-11-27 18:12:43 +0000

    move nxt level into a fn

--- pages/api/chapter/[book]/[chapter].js
+++ pages/api/chapter/[book]/[chapter].js
@@ -4,6 +4,7 @@ import { getBookChapter} from '../../line/_core';
 async function handler(req, res) {
     if (req.method === 'GET') {
         const { book, chapter } = req.query;
+        console.log('getting book chapter', book, chapter);
         const data = await getBookChapter(book, chapter);
         return res.json(data);
     } 
--- pages/api/line/_core.js
+++ pages/api/line/_core.js
@@ -2,7 +2,6 @@ import getDb from "../../../mongo";
 import { ObjectId } from 'mongodb';
 import { getWordforAllText } from "../word/_core";
 import _ from 'lodash-es';
-import { KeyboardSharp } from "@mui/icons-material";
 
 export const getLineByTextAndLanguage = async (text, language) =>{
     const db = await getDb();
@@ -109,28 +108,45 @@ const getNextLevel = (context) => {
     return `L${max+1}`;
 };
 
+const getNextContext = async (context) => {
+    const max = _.keys(context).length;
+};
+
+const getMinNextLevel = async (book, context) => {
+    const db = await getDb();
+    const queryFormat = convertToQueryFormat(context, 'context');
+    const nextLevel = getNextLevel(context);
+    const agg = await db.collection('lines')
+        .aggregate([
+            {
+                $match: {
+                    book,
+                    ...queryFormat
+                }
+            },
+            {
+                $group: {
+                    _id: null,
+                    minNextLevel: { $min: `$context.${nextLevel}`}
+                }
+            }
+        ]).toArray();
+    console.log(agg);
+    const nxt = agg.length ? agg[0].minNextLevel: null;
+    return nxt;
+};
+
 export const getBookChapter = async (book, chapter) => {
     const db = await getDb();
     try {
         const context = extractBookContext(chapter);
+        console.log(context);
         const queryFormat = convertToQueryFormat(context, 'context');
+        console.log(queryFormat);
         const nextLevel = getNextLevel(context);
-        const agg = await db.collection('lines')
-            .aggregate([
-                {
-                    $match: {
-                        book,
-                        ...queryFormat
-                    }
-                },
-                {
-                    $group: {
-                        _id: null,
-                        minNextLevel: { $min: `$context.${nextLevel}`}
-                    }
-                }
-            ]).toArray();
-        const nxt = agg.length ? agg[0].minNextLevel: null;
+        console.log(nextLevel);
+        const nxt = await getMinNextLevel(book, context);
+        console.log(nxt);
         const query = {   
             book,
             ...queryFormat
@@ -138,12 +154,14 @@ export const getBookChapter = async (book, chapter) => {
         if(nxt !== null) {
             query[`context.${nextLevel}`] = nxt;
         }
+        console.log('qery',query);
         const lines = await db.collection('lines').find( query,
             { projection: {createdBy: 0, createdAt: 0} }
         ).toArray();
         const chp = nxt ? `${chapter}.${nxt}`: chapter;
         return { lines, chapter: chp };
     } catch(e) {
+        console.log(e);
         return e;
     }
 };
