3c4e8ba523feeb30575ef4ec31f0540a7c8c6218
Ganaraj Permunda <ganarajpr@gmail.com>
2022-06-25 17:23:53 +0100

    adding seq and context

--- pages/api/line/index.js
+++ pages/api/line/index.js
@@ -3,10 +3,36 @@ import corsWrapper from "../../../lib/corsWrapper";
 import { getSession } from 'next-auth/client';
 import { updateLine, updateLineText } from "./_core";
 import { ServerSession } from "mongodb";
+
+const contextify = (a) => {
+    const pows = [12, 10, 7, 4, 0];
+    return a.reduce((prev,cur,index) => {
+        const i = pows.length - a.length + index;
+        prev = prev+cur*Math.pow(10,pows[i]);
+        return prev;
+    },0);
+};
+
+const extractSequence = (bookContext) => {
+    const context = bookContext.split('.');
+    return contextify(context);
+};
+
+const convertToContextLevels = (bookContext) => {
+    const context = bookContext.split('.');
+    const levels = context.reduce((prev,cur,index) => {
+        prev[`L${index+1}`] = +cur;
+        return prev;
+    },{});
+    return levels;    
+};
+
+
 const addLine = async (text,language,script,book, bookContext, user) => {
     const db = await getDb();
     const lines = db.collection('lines');
-    const line = await lines.findOne({text, language});
+    const line = await lines.findOne({ text, language });
+    const context = convertToContextLevels(bookContext);
     if(!user) {
         throw(new Error('User not found'));
     }
@@ -17,6 +43,8 @@ const addLine = async (text,language,script,book, bookContext, user) => {
             script,
             book,
             bookContext,
+            sequence: extractSequence(bookContext),
+            context,
             createdBy: user.id,
             createdAt: new Date(),
             approved: false
