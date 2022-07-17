c8274209a34c23e48b79da663967ac29d7c58482
Ganaraj Permunda <ganarajpr@gmail.com>
2022-07-17 21:08:45 +0100

    adding chapter page

--- pages/book/[name]/chapter/[chpNo].js
+++ pages/book/[name]/chapter/[chpNo].js
@@ -1,41 +1,44 @@
 import { getBookChapter, getLine } from "../../../../fetches/line";
-import Layout from '../../../../components/Layout';
-import Container from '@mui/material/Container';
-import Link from 'next/link';
-import _ from 'lodash-es';
-import Head from 'next/head';
-import Box from '@mui/material/Box';
-import Paper from '@mui/material/Paper';
-import Typography from '@mui/material/Typography';
-import Card from '@mui/material/Card';
-import CardActionArea from '@mui/material/CardActionArea';
-import CardContent from '@mui/material/CardContent';
-import { getSum } from '../../../../lib/util';
-import { Divider } from "@mui/material";
+import Layout from "../../../../components/Layout";
+import Link from "next/link";
+import _ from "lodash-es";
+import Head from "next/head";
+import Typography from "@mui/material/Typography";
+import { getSum } from "../../../../lib/util";
 import LanguageText from "../../../../components/LanguageText";
-import Sanscript from '@sanskrit-coders/sanscript';
+import Sanscript from "@sanskrit-coders/sanscript";
+import ChevronLeftButton from "../../../../components/ChevronLeftButton";
+import ChevronRightButton from "../../../../components/ChevronRightButton";
 
 const getLines = (para) => {
-    const lines = para.text.split('\n');
+    const lines = para.text.split("\n");
     return (
-        <Card variant="outlined" sx={{ overflowWrap: "break-word" }} data-test="shloka">
-            <Link href={`/book/${para.book}/${para.bookContext}`}>
-                <CardActionArea>
-                    <CardContent>
+        <Link href={`/book/${para.book}/${para.bookContext}`}>
+            <div className="grid grid-flow-col">
+            <div className="grid justify-center self-center mr-6">
+                {para.bookContext}
+            </div>
+            <div className="grid grid-flow-row justify-start">
                         {lines.map((line) => {
-                            return (<Typography variant="h5" component="div" key={line} data-test="line">
-                                <LanguageText source={para.script}>{line}</LanguageText>
-                            </Typography>);
+                            return (
+                                <h5 className="text-2xl"
+                                    key={line}
+                                    data-test="line"
+                                >
+                                    <LanguageText source={para.script}>
+                                        {line}
+                                    </LanguageText>
+                                </h5>
+                            );
                         })}
-                        <Typography sx={{ mb: 1.5, textAlign: 'right' }} color="text.secondary" data-test="bookContext">
-                            { para.bookContext}
-                        </Typography>
-                    </CardContent>
-                </CardActionArea>
+                    
+            </div>
+            
+            </div>
+                
             </Link>
-        </Card>
     );
-}
+};
 
 const displayLines = (paragraphs) => {
     paragraphs.sort((a, b) => {
@@ -43,104 +46,153 @@ const displayLines = (paragraphs) => {
         const numb = getSum(b.bookContext);
         return numa - numb;
     });
-    return paragraphs.map( para => {
+    return paragraphs.map((para) => {
         return (
-            <Box key={para.bookContext} sx={{
-                flexGrow: 1,
-                justifyContent: 'center',
-                textAlign: 'center',
-            }}
-        >
-            {getLines(para)}
-        </Box>
-        )
-        
+            <div key={para.bookContext} className="grid grid-flow-row container mx-auto justify-start p-5 border-b cursor-pointer hover:shadow-md hover:bg-pink-900 hover:text-white">
+                {getLines(para)}
+            </div>
+        );
     });
 };
 
-export default function Book({ lines, bookName, chapter, nextContext, prevContext }) {
-
+export default function Book({
+    lines,
+    bookName,
+    chapter,
+    nextContext,
+    prevContext,
+}) {
     return (
-        <Layout>
-            <Head>
-                <title>Smrthi - {bookName} - {chapter}</title>
-                <meta name="description" content={ "Chapters of " + Sanscript.t(bookName, 'hk', 'devanagari') }/>
-                <meta property="og:type" content="website"/>
-                <meta property="og:url" content={`https://www.smrthi.com/book/${bookName}/chapter/${chapter}`}/>
-                <meta property="og:title" content={`Smrithi - ${Sanscript.t(bookName, 'hk', 'devanagari')} `}/>
-                <meta property="og:description" content={ "Chapter " + chapter + " of " + Sanscript.t(bookName, 'hk', 'devanagari') }/>
-                <meta property="og:image" content={`https://www.smrthi.com/logo.jpg`}/>
-                <meta property="twitter:card" content="summary"/>
-                <meta property="twitter:url" content={`https://www.smrthi.com/book/${bookName}/chapter/${chapter}`}/>
-                <meta property="twitter:title" content={`Chapter ${chapter} of ${Sanscript.t(bookName, 'hk', 'devanagari')}`}/>
-                <meta property="twitter:description" content={ "Chapter "+ chapter + " of " + Sanscript.t(bookName, 'hk', 'devanagari') }/>
-                <meta property="twitter:image" content={`https://www.smrthi.com/logo.jpg`}/>
-            </Head>
-            <Container maxWidth="lg">
-                <Box
-                    sx={{
-                            flexGrow: 1,
-                            justifyContent: 'center',
-                            textAlign: 'center'
-                        }}
-                    >
-                    <Paper elevation={1} sx={{pb: 1}}>
-                        <Link href={`/book/${lines[0].book}`}>
-                            <Box sx={{ display: "flex", flexGrow: 1, justifyContent: "center", 
-                                'cursor': 'pointer', ':hover': { color: '#666'}}}>
-                                <img src="/smrthi-symbol.png" height="50"/>
-                                <Typography variant="h4" component="h4" sx={{ color: '#999', ml: 2, mt: 1}} data-test="bookContext">
-                                    <LanguageText source="hk">{lines[0].book}</LanguageText>
-                                        {' ' + chapter}      
-                                </Typography>
-                            </Box>
-                        </Link>
-                    </Paper>
-                </Box>
-                {displayLines(lines)} 
+        <>
+            <Layout>
+                <Head>
+                    <title>
+                        Smrthi - {bookName} - {chapter}
+                    </title>
+                    <meta
+                        name="description"
+                        content={
+                            "Chapters of " +
+                            Sanscript.t(bookName, "hk", "devanagari")
+                        }
+                    />
+                    <meta property="og:type" content="website" />
+                    <meta
+                        property="og:url"
+                        content={`https://www.smrthi.com/book/${bookName}/chapter/${chapter}`}
+                    />
+                    <meta
+                        property="og:title"
+                        content={`Smrithi - ${Sanscript.t(
+                            bookName,
+                            "hk",
+                            "devanagari"
+                        )} `}
+                    />
+                    <meta
+                        property="og:description"
+                        content={
+                            "Chapter " +
+                            chapter +
+                            " of " +
+                            Sanscript.t(bookName, "hk", "devanagari")
+                        }
+                    />
+                    <meta
+                        property="og:image"
+                        content={`https://www.smrthi.com/logo.jpg`}
+                    />
+                    <meta property="twitter:card" content="summary" />
+                    <meta
+                        property="twitter:url"
+                        content={`https://www.smrthi.com/book/${bookName}/chapter/${chapter}`}
+                    />
+                    <meta
+                        property="twitter:title"
+                        content={`Chapter ${chapter} of ${Sanscript.t(
+                            bookName,
+                            "hk",
+                            "devanagari"
+                        )}`}
+                    />
+                    <meta
+                        property="twitter:description"
+                        content={
+                            "Chapter " +
+                            chapter +
+                            " of " +
+                            Sanscript.t(bookName, "hk", "devanagari")
+                        }
+                    />
+                    <meta
+                        property="twitter:image"
+                        content={`https://www.smrthi.com/logo.jpg`}
+                    />
+                </Head>
+                <Link href={`/book/${lines[0].book}`}>
+                    <div className="grid grid-flow-col justify-self-center gap-2 cursor-pointer">
+                        <img className="w-14" src="/smrthi-symbol.png" />
+                        <p
+                            className="text-4xl inline justify-center self-center text-slate-700"
+                            data-test="bookLocation"
+                        >
+                            <LanguageText source="hk">
+                                {lines[0].book}
+                            </LanguageText>
+                            {" " + chapter}
+                        </p>
+                    </div>
+                </Link>
+                <div className="grid mt-8 mb-20 mx-4">
+                    <div className="grid grid-flow-row">
+                        {displayLines(lines)} 
+                    </div>
+                </div>
 
-                <Divider sx={{mt: 2, mb: 2}}/> 
-                <Box
-                    sx={{
-                            flexGrow: 1
-                        }}
+            </Layout>
+            <div className="grid grid-flow-col w-full h-16 border-4 bottom-0 fixed bg-pink-900 text-white">
+                {prevContext && (
+                    <Link
+                        href={`/book/${lines[0].book}/chapter/${prevContext}`}
                     >
-                     { prevContext &&
-                        <Link href={`/book/${lines[0].book}/chapter/${prevContext}`}>
-                            <Typography variant="h5" component="h5" data-test="prev" 
-                                sx={{'cursor': 'pointer', display: 'inline-block', color: 'primary.main', ':hover': { color: 'secondary.main'}, textAlign: 'left'}}>
-                                Prev        
-                            </Typography>
-                        </Link>
-                    }
-                    {
-                        nextContext && 
-                        <Link href={`/book/${lines[0].book}/chapter/${nextContext}`}>
-                            <Typography variant="h5" component="h5" data-test="next"
-                                sx={{'cursor': 'pointer', display: 'inline-block', color: 'primary.main', ':hover': { color: 'secondary.main'}, float: 'right'}}>
-                                Next        
-                            </Typography>
-                        </Link> 
-                    }
-                    
-                </Box>                
-            </Container> 
-        </Layout>
+                        <ChevronLeftButton
+                            data-test="prevContext"
+                            className="justify-self-start grid grid-flow-col place-items-center text-2xl font-bold ml-5 p-1 hover:bg-white hover:text-pink-900"
+                        >
+                            Previous
+                        </ChevronLeftButton>
+                    </Link>
+                )}
+                {nextContext && (
+                    <Link
+                        href={`/book/${lines[0].book}/chapter/${nextContext}`}
+                    >
+                        <ChevronRightButton
+                            data-test="nextContext"
+                            className="justify-self-end grid grid-flow-col place-items-center font-bold text-2xl mr-5 p-1 hover:bg-white hover:text-pink-900"
+                        >
+                            Next
+                        </ChevronRightButton>
+                    </Link>
+                )}
+            </div>
+        </>
     );
 }
 
-
-
 export async function getServerSideProps(context) {
-  const { name, chpNo } = context.params;
-  const { lines, chapter, nextContext, prevContext }    = await getBookChapter(name, chpNo);
-  return {
-    props: {
-      lines,
-      chapter,
-      bookName: name,
-      nextContext,
-      prevContext
-    }
- };
+    const { name, chpNo } = context.params;
+    const { lines, chapter, nextContext, prevContext } = await getBookChapter(
+        name,
+        chpNo
+    );
+    return {
+        props: {
+            lines,
+            chapter,
+            bookName: name,
+            nextContext,
+            prevContext,
+        },
+    };
 }
