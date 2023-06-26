1344d4c60e168d2b574eb32fdeb14ca754a8ef70
Ganaraj Permunda <ganarajpr@gmail.com>
2023-06-26 23:34:27 +0100

    fixing base url

--- pages/api/image/[book]/[bookContext].js
+++ pages/api/image/[book]/[bookContext].js
@@ -5,9 +5,8 @@ import { getScreenshot } from '../../../../lib/chromium';
 async function handler(req, res) {
     if (req.method === 'GET') {
         const { book, bookContext } = req.query;
-        const baseUrl = process.env.NEXTAUTH_URL;
         const finalBookContext = bookContext.replace('.jpg', '');
-        const url = `${baseUrl}/embed/${book}/${finalBookContext}`;
+        const url = `https://smrthi.com/embed/${book}/${finalBookContext}`;
         const isDev = process.env.DEV === 'true';
         const file = await getScreenshot(url, isDev);
         res.statusCode = 200;
